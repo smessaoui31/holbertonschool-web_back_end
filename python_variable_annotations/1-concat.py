19ed9d3cb50b20baa91ddf2fd9bee57a8bf7fb72
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-29 13:47:53 +0000

    Concat two string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+"""
+Module that takes two strings as arguments
+and returns a concatenated string
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concat two strings and return them concatenated
+
+    Args:
+
+        1 (string) : First string.
+        2 (string) : Second string.
+
+    Returns :
+        string : The two strings concatenated.
+    """
+    return str1 + str2
