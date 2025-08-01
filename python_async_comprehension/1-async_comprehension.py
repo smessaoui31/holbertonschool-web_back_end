917917d82197bed19e7dafa167b1de726bfc5204
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-01 07:11:04 +0000

    FIX the import'name

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,7 +2,7 @@
 """Async comprehension to collect values from async_generator."""
 
 from typing import List
-from 0-async_generator import async_generator
+from async_generator import async_generator
 
 
 async def async_comprehension() -> List[float]:
