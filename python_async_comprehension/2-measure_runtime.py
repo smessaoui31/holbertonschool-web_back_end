9efc54ca5e2651b6104368cd4fd27376108fadbc
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-01 07:16:49 +0000

    Fix the import and modify variable's name

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,10 +3,12 @@
 Measure execution time of running async_comprehension
 4 times in parallel.
 """
-
+from typing import Generator, List
 import asyncio
 import time
-from 1-async_comprehension import async_comprehension
+import random
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
@@ -16,7 +18,8 @@ async def measure_runtime() -> float:
     Returns:
         float: Total runtime in seconds.
     """
-    start_time = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end_time = time.perf_counter()
-    return end_time - start_time
+    start: float = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end: float = time.time()
+
+    return end - start
