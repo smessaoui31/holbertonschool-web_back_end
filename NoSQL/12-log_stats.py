77330b0a3f819934b7ebb58272b04ffcccd85f8f
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-15 21:06:26 +0000

    FIX some method

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,27 +1,48 @@
 #!/usr/bin/env python3
 """
-Provides some stats about Nginx logs stored in MongoDB.
-Database: logs
-Collection: nginx
+12-log_stats.py
+Script providing statistics about Nginx logs stored in MongoDB.
 """
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def main():
+    """
+    Provides statistics about Nginx logs stored in MongoDB.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    cursor = nginx_collection.find({'method': 'GET'})
+    get_count = len(list(cursor))
 
-    
-    print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    cursor = nginx_collection.find({'method': 'POST'})
+    post_count = len(list(cursor))
 
-    status_checks = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_checks} status check")
+    cursor = nginx_collection.find({'method': 'PUT'})
+    put_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'PATCH'})
+    patch_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'method': 'DELETE'})
+    delete_count = len(list(cursor))
+
+    cursor = nginx_collection.find({'path': '/status'})
+    status = len(list(cursor))
+
+    cursor = nginx_collection.find()
+    total = len(list(cursor))
+
+    print('{} logs'.format(total))
+    print('Methods:')
+    print('\tmethod GET: {}'.format(get_count))
+    print('\tmethod POST: {}'.format(post_count))
+    print('\tmethod PUT: {}'.format(put_count))
+    print('\tmethod PATCH: {}'.format(patch_count))
+    print('\tmethod DELETE: {}'.format(delete_count))
+    print('{} status check'.format(status))
+
+
+if __name__ == "__main__":
+    main()
