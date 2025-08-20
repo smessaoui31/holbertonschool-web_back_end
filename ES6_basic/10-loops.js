d6bc429ca7751358a10b396fcc5d274cf3d02ee6
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-20 11:33:29 +0000

    ES6 method with the FOR of operator

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var idx in array) {
-    var value = array[idx];
-    array[idx] = appendString + value;
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
   }
-
-  return array;
+  return result;
 }
