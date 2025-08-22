f0108d08c397effc8057cfb108f5f151de6efc8c
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-22 20:01:15 +0000

    Vroom Vroom

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,12 @@
+/* eslint no-underscore-dangle: 0 */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
