a48a7821f10aa644f74342ac09ffcc9661b907cd
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-25 14:50:48 +0000

    FEAT The space after the return

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 function getListStudents () {
-    return[
+    return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
         { id: 3, firstName: 'Serena', location: 'San Francisco' },
