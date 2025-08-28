c2afdccde5c78064d75306606ecad4556b83738e
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-08-28 11:55:51 +0000

    Task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+}
+const result = [];
+
+for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)){
+        result.push(value.slice(startString.length));
+    }
+}
+
+return result.join('-');
+}
