df149c82935733226d69e4925cae69a21706b97a
Sofian <messaoui.sofian@hotmail.fr>
2026-09-04 12:38:38 +0000

    FEAT: add type hints to filter_datum function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 Main file
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                  separator: str) -> str:
     """Returns the log message obfuscated"""
     pattern = r'({})=[^{}]*'.format('|'.join(fields), separator)
     return re.sub(pattern, r'\1={}'.format(redaction), message)
